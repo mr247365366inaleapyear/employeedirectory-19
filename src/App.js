95f7b635f7a351bd988a837ebc0036223e291922
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-21 14:06:31 -0500

    editing css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import './App.css';
+import SearchResultContainer from "./components/SearchResultContainer";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <SearchResultContainer />;
 }
 
 export default App;
